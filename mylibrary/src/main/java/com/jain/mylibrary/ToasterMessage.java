dbfa05f9c31a3b162da0fc8a948961a357fa8212
Ankit <ankit.jain@duronto.in>
2021-01-07 16:00:04 +0530

    'fixes'

--- mylibrary/src/main/java/com/jain/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/jain/mylibrary/ToasterMessage.java
@@ -8,19 +8,22 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class ToasterMessage {
+    private static String API_KEY;
+//    String API_KEY="api_key";
 
     public static void s(Context c, String message){
         try {
             ApplicationInfo ai = null;
             ai = c.getPackageManager().getApplicationInfo(c.getPackageName(), PackageManager.GET_META_DATA);
             Bundle bundle = ai.metaData;
-            if(bundle.containsKey("my_test_metagadata")){
-                String myApiKey = bundle.getString("my_test_metagadata");
+            if(bundle.containsKey(API_KEY)){
+                String myApiKey = bundle.getString(API_KEY);
                 Toast.makeText(c,"myApiKey: "+myApiKey,Toast.LENGTH_SHORT).show();
             }else{
                 Toast.makeText(c,"Dear developer. " +
-                        "Don't forget to configure <meta-data android:name=\"my_test_metagadata\" android:value=\"testValue\"/> in your AndroidManifest.xml file.",Toast.LENGTH_SHORT).show();
+                        "Don't forget to configure <meta-data android:name=\"api_key\" android:value=\"your_api_key\"/> in your AndroidManifest.xml file.",Toast.LENGTH_SHORT).show();
 
+            return;
             }
 
         } catch (Exception e) {
